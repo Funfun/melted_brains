c683ea9785fe5e3faf989575d6b43840d442f9db
Tsyren Ochirov <nsu1team@gmail.com>
2015-01-25 00:03:26 +0100

    Conner cases for client side;

--- static/js/core.js
+++ static/js/core.js
@@ -16,24 +16,53 @@ function MacOsXCharCode(charCode){
   }
   return charCode;
 }
+function numberOFDupInStr(str){
+  var res = str.match(/(\t{1,})/g);
+
+  if(str[0] == "\t" && res){
+    console.log("Tab first", str[0] == "\t");
+    console.log("Tab size", res[0].length);
+    return res[0].length;
+  }
+  else {
+    return 0;
+  }
+
+}
 
 function onKeyPress(pEvent){
   var charCode = pEvent.charCode,
-      newOffset = null;
-
-  console.log(window.tokenElem.value);
-  console.log(charCode);
-  if(window.tokenElem.value.charCodeAt(window.tokenElem.offSet) == MacOsXCharCode(charCode)){
-    console.log('correct', charCode, window.tokenElem.value, window.tokenElem.offSet)
+      newOffset = null,
+      isNextTab,
+      i;
+  if(MacOsXCharCode(charCode) == window.tokenElem.value.charCodeAt(window.tokenElem.offSet)){
+    isNextTabs = numberOFDupInStr(window.tokenElem.value.substr(window.tokenElem.offSet+1));
     newOffset = window.tokenElem.offSet++;
-    window.tokenElem.el.innerHTML = window.tokenElem.value.substr(0, newOffset+1) + "|" + window.tokenElem.value.substr(newOffset+1);
-    console.log(newOffset, window.tokenElem.value.length);
-    if( (newOffset+1) >= window.tokenElem.value.length){
-      console.log("Next");
 
+    if(isNextTabs > 0){
+      window.tokenElem.offSet = window.tokenElem.offSet + isNextTabs;
+      newOffset = window.tokenElem.offSet - 1;
+    }
+
+    window.tokenElem.el.innerHTML = setCarretAndSkipTab(newOffset);
+
+    if( (newOffset+1) >= window.tokenElem.value.length || window.tokenElem.value == "&amp;"){
+      window.tokenElem.el.innerHTML = window.tokenElem.value;
       window.tokenElem = chooseNextAt(window.tokenElem.index+1);
+      var str = window.tokenElem.value;
+      window.tokenElem.el.innerHTML = setCarret(str);
     }
   }
+}
+function setCarretAndSkipTab(offSet){
+  // case01
+  if(window.tokenElem.value == "&amp;"){
+    return (window.tokenElem.value + "|");
+  }
+  return window.tokenElem.value.substr(0, offSet+1) + "|" + window.tokenElem.value.substr(offSet+1);
+}
+function setCarret(str){
+  return "|" + str;
 }
 function chooseNextAt(idx){
   console.log("idx", idx);
