17667fa43e5d1fa1ab7b52ddd7a9b0c768a5cdef
Tsyren Ochirov <nsu1team@gmail.com>
2015-01-24 13:13:39 +0100

    Parse code from pre-text.

--- static/js/core.js
+++ static/js/core.js
@@ -1,7 +1,16 @@
 function set_cursor(){
-
+  // skip comment & new lines
+  var elements = arguments[0].getElementsByTagName('span');
+  var collected = [];
+  for(var i=0; i<elements.length; i++){
+    if(["com", "pln"].indexOf(elements[i].className)==-1 ){
+      collected.push(elements[i]);
+    }
+  }
+  // TODO: set cursor on first element
 }
 function onLoad(){
-
+  var codeBlock = document.getElementById('code');
+  set_cursor(codeBlock);
 }
 window.onload = onLoad;
